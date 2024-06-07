8992905847cfed038c884e302a579dca3a34e11a
aryapanchlinge <105349177+aryapanchlinge@users.noreply.github.com>
2024-06-07 21:32:51 +0000

    comparison

---
+++ 01_basic/04_comparisons.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+
+console.log("2">1);//true           //avoid this type of comparison
+console.log("02">1);//true              
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+console.log(undefined>0);//false
+console.log(undefined==0);//false
+console.log(undefined>=0);//true
+
+//=== //strictcheck =checks datatypes too
+console.log("2" ===2);
