af8c4a0122819752af6bd624cc3ab89c5ef4d5b6
aryapanchlinge <105349177+aryapanchlinge@users.noreply.github.com>
2024-06-07 21:06:15 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+let name ="arya"
+let age = "21"
+let isLoggedin="False"
+
+// number = 2 to power 53
+// bigint 
+// string = ""
+// boolean = true/false
+// null = stand alone value
+// undefined = value is not asigned
+// symbol = unique
+
+
+// object 
+console.log(typeof "arya"); // typeof is used to find the datatype
+console.log(typeof null); // object
+console.log(typeof undefined);//undefined
